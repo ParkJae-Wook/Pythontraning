fe64b0da965e70de231c4a1356b1ed10c4e60c43
JaeWookPark <herojw504@gmail.com>
2024-12-30 14:30:29 +0900

    Update number_guessing_game.py

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -20,115 +20,97 @@ class RunGame:
         entry.delete(0, "end")
         entry.focus()
         start_button.config(text="다시 시작")
+   
     def __init__(self):
         self.mynum = 0
         self.com_num = 0
         self.guess_num = 0
         self.turn = 0
         self.min = 1
-        self.max = 100        
-    def set_mynum(self,num):
-        self.mynum = num
-    def set_comnum(self,num):
-        self.com_num = num
-    def set_guessnum(self,num):
-        self.guess_num = num
-    def set_turn(self,num):
-        self.turn = num
-    def set_min(self,num):
-        self.min = num
-    def set_max(self,num):
-        self.max = num
-    def get_mynum(self):
-        return self.mynum
-    def get_comnum(self):
-        return self.com_num
-    def get_guessnum(self):
-        return self.guess_num
-    def get_turn(self):
-        return self.turn
-    def get_min(self): 
-        return self.min
-    def get_max(self):
-        return self.max
+        self.max = 100
+        up_button.config(state="normal")
+        down_button.config(state="normal")
     
-def check_guess():
-    com_num = int(run_game.get_comnum)
-    n = int(run_game.get_guessnum)
-    mn = int(run_game.get_mynum)
-    if n < com_num:
-        input_label.config(text="UP!")
-    elif n > com_num:
-        input_label.config(text="DOWN!")    
-    else:
-        messagebox.showinfo("알림", "정답입니다!")
-        return
+    def check_guess(self):
+        if self.guess_num < self.com_num:
+            input_label.config(text="UP!")
+        elif self.guess_num > self.com_num:
+            input_label.config(text="DOWN!")    
+        else:
+            messagebox.showinfo("알림", "정답입니다!")
+            return
                       
-    com_guess = random.randint(run_game.get_min, run_game.get_max)
-    com_num_label.config(text=str(com_guess))
-    if com_guess == mn:
-        messagebox.showinfo("알림", "컴퓨터가 정답을 맞췄습니다!")
-        return
-    com_label.config(text="UP or DOWN?")
+        self.com_guess = random.randint(self.min, self.max)
+        com_num_label.config(text=str(self.com_guess))
+        if self.com_guess == self.mynum:
+            messagebox.showinfo("알림", "컴퓨터가 정답을 맞췄습니다!")
+            return
+        com_label.config(text="UP or DOWN?")
 
-def on_enter(event):
-    
-    if label.cget("text") == "1부터 100 사이의 숫자를 맞춰보세요.":
-        messagebox.showinfo("알림", "게임을 시작해주세요")
-        return
-    elif mynum_label.cget("text") == "0":
-        try:
-            run_game.set_mynum(int(entry.get()))
-        except ValueError:
-            messagebox.showinfo("알림", "숫자를 입력해주세요.")
+    def on_enter(self,event):
+        
+        if label.cget("text") == "1부터 100 사이의 숫자를 맞춰보세요.":
+            messagebox.showinfo("알림", "게임을 시작해주세요")
+            return
+        elif self.mynum == 0:
+            try:
+                self.mynum=int(entry.get())
+            except ValueError:
+                messagebox.showinfo("알림", "숫자를 입력해주세요.")
+                entry.delete(0, "end")
+                entry.focus()
+                return
+            mynum_label.config(text=entry.get())
+            label.config(text="컴퓨터가 맞춰야 할 숫자")
+            input_num_label.config(text="뭐게?")
             entry.delete(0, "end")
             entry.focus()
             return
-        mynum_label.config(text=entry.get())
-        label.config(text="컴퓨터가 맞춰야 할 숫자")
-        input_num_label.config(text="뭐게?")
-        entry.delete(0, "end")
-        entry.focus()
-        return
-    elif run_game.get_turn() :
-        messagebox.showinfo("알림", "UP! 또는 DOWN! 버튼을 눌러주세요.")
-        return
-    else:
-        try:
-            run_game.set_guessnum(int(entry.get()))
-        except ValueError:
-            messagebox.showinfo("알림", "숫자를 입력해주세요.")
+        elif self.turn == 1:
+            messagebox.showinfo("알림", "UP! 또는 DOWN! 버튼을 눌러주세요.") #컴퓨터 턴일때 숫자를 입력하면 오류 출력
+            return
+        else:
+            try:
+                self.guess_num=int(entry.get())
+            except ValueError:
+                messagebox.showinfo("알림", "숫자를 입력해주세요.")
+                entry.delete(0, "end")
+                entry.focus()
+                return
+            input_num_label.config(text=entry.get())
+            self.check_guess()
+            self.turn=1
             entry.delete(0, "end")
             entry.focus()
-            return
-        input_num_label.config(text=entry.get())
-        check_guess()
-        run_game.set_turn(1)
-        entry.delete(0, "end")
-        entry.focus()
 
-def on_click(event):
-    text = event.widget.cget("text")
-    if text == "UP!":
-        if int(com_num_label.cget("text")) < int(mynum_label.cget("text")):
+    def on_click(self,event):
+        text = event.widget.cget("text")
+        if text == "시작" or text == "다시 시작":
+            self.start_game()
+        elif label.cget("text") == "1부터 100 사이의 숫자를 맞춰보세요.":
+            messagebox.showinfo("알림", "게임을 시작해주세요")
+            return
+        elif self.mynum == 0:
+            messagebox.showinfo("알림", "컴퓨터가 맞출 숫자를 입력해주세요.")
+            return   
+        elif self.turn == 0:
+            messagebox.showinfo("알림", "당신 차례입니다. 숫자를 입력해주세요.")
+            up_button.config(state="normal")
+            down_button.config(state="normal")
+            return
+        elif text == "UP!" and self.com_guess < self.mynum:
             com_label.config(text="UP!")
-            run_game.set_max(int(com_num_label.cget("text")))
-            run_game.set_turn(0)
-        else:
-            messagebox.showinfo("알림", "잘못된 입력입니다.")
-            return
-    elif text == "DOWN!":
-        if int(com_num_label.cget("text")) > int(mynum_label.cget("text")):
+            self.min=int(com_num_label.cget("text"))
+            self.turn=0            
+        elif text == "DOWN!" and self.com_guess > self.mynum:
             com_label.config(text="DOWN!")
-            run_game.set_min(int(com_num_label.cget("text")))
-            run_game.set_turn(0)
+            self.max=int(com_num_label.cget("text"))
+            self.turn=0
         else:
             messagebox.showinfo("알림", "잘못된 입력입니다.")
+            up_button.config(state="normal")
+            down_button.config(state="normal")
             return
-    elif text == "시작" or text == "다시 시작":
-        run_game.start_game()
-    else:
-        print("Error")
         
 
     
@@ -162,21 +144,24 @@ input_label.grid(row=3, column=0,columnspan=2,sticky='news')
 
 entry = tk.Entry(window, font=("Arial", 15),width=3)
 entry.grid(row=4, column=1,columnspan=2,sticky='news')
-entry.bind("<Return>", on_enter)
+
 #입력칸
 
 
 up_button = tk.Button(window, text='UP!', font=("Arial", 15),width=3, bg="lightgray", fg="black")
 up_button.grid(row=5, column=0,columnspan=2,sticky ='news' , padx=2, pady=2)
-up_button.bind("<Button-1>", on_click)
+
 down_button = tk.Button(window, text='DOWN!', font=("Arial", 15),width=3, bg="lightgray", fg="black")
 down_button.grid(row=5, column=2,columnspan=2,sticky ='news' , padx=2, pady=2)
-down_button.bind("<Button-1>", on_click)
+
 start_button = tk.Button(window, text='시작', font=("Arial", 15),width=3, bg="lightgray", fg="black")
 start_button.grid(row=6, column=0,columnspan=4,sticky ='news' , padx=2, pady=2)
-start_button.bind("<Button-1>", on_click)
+
 #버튼들
 
 run_game = RunGame()
-
+entry.bind("<Return>", run_game.on_enter)
+up_button.bind("<Button-1>", run_game.on_click)
+down_button.bind("<Button-1>", run_game.on_click)
+start_button.bind("<Button-1>", run_game.on_click)
 window.mainloop()
